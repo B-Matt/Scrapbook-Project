9412ca078f6f7b2df4f4b9eef978cf35e7b98834
Matej <b-matt@net.hr>
2016-11-26 23:04:00 +0100

    Add files via upload

--- database/scrapbook.sql
+++ database/scrapbook.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 22, 2016 at 03:55 PM
+-- Generation Time: Nov 26, 2016 at 11:04 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.6.24
 
@@ -37,14 +37,6 @@ CREATE TABLE `accounts` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `accounts`
---
-
-INSERT INTO `accounts` (`id`, `name`, `password`, `email`, `avatar`, `bio`, `created_at`, `updated_at`) VALUES
-(1, 'B-Matt', 'fd9d94340dbd72c11b37ebb0d2a19b4d05e00fd78e4e2ce8923b9ea3a54e900df181cfb112a8a73228d1f3551680e2ad9701a4fcfb248fa7fa77b95180628bb2', 'test@gmail.com', 'https://scontent-vie1-1.xx.fbcdn.net/v/t1.0-9/13342896_105999229824012_7010689265060832149_n.jpg?oh=c99693ec80945a70a20604395a6b0556&oe=58C4355D', 'Croatia | Programmer | Scrapbook CEO', '2016-11-12 14:36:46', '2016-11-12 14:36:46'),
-(2, 'Tester', 'fd9d94340dbd72c11b37ebb0d2a19b4d05e00fd78e4e2ce8923b9ea3a54e900df181cfb112a8a73228d1f3551680e2ad9701a4fcfb248fa7fa77b95180628bb2', 'tester@gmail.com', NULL, NULL, '2016-11-21 20:24:50', '2016-11-21 20:24:50');
-
 -- --------------------------------------------------------
 
 --
@@ -59,14 +51,6 @@ CREATE TABLE `comments` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `comments`
---
-
-INSERT INTO `comments` (`id`, `poster_id`, `image_id`, `text`, `created_at`, `updated_at`) VALUES
-(1, 2, 1, 'tester', '2016-11-21 20:25:49', '2016-11-21 20:25:49'),
-(2, 1, 1, '&lt;3', '2016-11-22 09:52:45', '2016-11-22 09:52:45');
 
 -- --------------------------------------------------------
 
@@ -83,18 +67,9 @@ CREATE TABLE `images` (
   `path` varchar(70) NOT NULL,
   `views` int(10) UNSIGNED NOT NULL DEFAULT '0',
   `loves` int(10) UNSIGNED NOT NULL DEFAULT '0',
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `images`
---
-
-INSERT INTO `images` (`id`, `poster_id`, `title`, `description`, `isPrivate`, `path`, `views`, `loves`, `created_at`, `updated_at`) VALUES
-(1, 1, 'Basketball', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis ex sceLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis ex scelerisque, convallis sem a, dignissim erat. Aenean auctor lectus in tortor lacinia, sit amet hen', 0, 'images/582edc0e661573bd66ef322c72df0b6368a085bc4e84c_full.jpg', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(2, 1, 'Sky', 'Sky!', 0, 'images/582ee6ca50b65b0d7c2f8cfcc3bf742be9d3e2acf624e_full.jpg', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(4, 1, 'Kids', 'Great time with my friends!', 0, 'images/58335d76db93ec6fed7fdfba9e9b82f26884aa1135433_full.jpg', 0, 0, '2016-11-21 19:47:51', '2016-11-21 19:47:51');
 
 -- --------------------------------------------------------
 
@@ -152,17 +127,17 @@ ALTER TABLE `accounts`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 --
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT for table `loves`
 --
 ALTER TABLE `loves`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
